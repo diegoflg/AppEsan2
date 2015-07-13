7f5f2ac3534fe99aa49672f3e2e97067ff851ad3
diegoflg <diegoflg2@hotmail.com>
2015-07-13 14:42:18 -0500

    agrege clase notas

---
+++ app/src/main/java/pe/edu/esan/appesan2/Notas.java
@@ -0,0 +1,20 @@
+package pe.edu.esan.appesan2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Diegoflg on 7/13/2015.
+ */
+public class Notas extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.lay_noticia, container, false);
+        return rootView;
+    }
+}
