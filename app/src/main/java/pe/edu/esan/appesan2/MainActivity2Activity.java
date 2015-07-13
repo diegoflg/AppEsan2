c05ca2ebe242912d0a1bcad4b791317105243045
diegoflg <diegoflg2@hotmail.com>
2015-07-13 15:17:52 -0500

    agrege los items en el main actiivity2activity

--- app/src/main/java/pe/edu/esan/appesan2/MainActivity2Activity.java
+++ app/src/main/java/pe/edu/esan/appesan2/MainActivity2Activity.java
@@ -62,7 +62,12 @@ public class MainActivity2Activity extends ActionBarActivity
                 fragment= new Horario();
                 break;
             case 2:
+                fragment= new Notas();
                 break;
+            case 3:
+                fragment= new Calendario();
+                break;
+
         }
         fragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
