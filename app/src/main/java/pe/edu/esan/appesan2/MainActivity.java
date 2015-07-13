9d04779af4b783f8bb780c186362df1da83a57a5
diegoflg <diegoflg2@hotmail.com>
2015-07-13 10:41:07 -0500

    Escribí un comentario en MainActivity.java y algo más

--- app/src/main/java/pe/edu/esan/appesan2/MainActivity.java
+++ app/src/main/java/pe/edu/esan/appesan2/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-/*Hola mundo cruel*/
+/*Hola mundo crueluighkuh*/
 
 
 public class MainActivity extends ActionBarActivity {
