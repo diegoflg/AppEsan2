58796ad1820e291215eb5df81b169de399c9e8a4
diegoflg <diegoflg2@hotmail.com>
2015-07-14 10:26:14 -0500

    agrege web view

--- app/src/main/java/pe/edu/esan/appesan2/Noticia.java
+++ app/src/main/java/pe/edu/esan/appesan2/Noticia.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by Diegoflg on 7/13/2015.
@@ -15,6 +16,17 @@ public class Noticia extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.lay_noticia, container, false);
+
+
+
+        View V = inflater.inflate(R.layout.lay_noticia, container, false);
+
+
+        WebView myWebView = (WebView) V.findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.google.com.pe");
+
         return rootView;
+
+
     }
 }
