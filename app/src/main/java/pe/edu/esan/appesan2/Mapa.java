d2d85474bb8e19cea266bee4776d59eff287a0d1
FiorelaDelPilar <fiorela2496@gmail.com>
2015-07-15 10:32:31 -0500

    Prueba de longitud y latitud de la universidad en el mapa 1.

--- app/src/main/java/pe/edu/esan/appesan2/Mapa.java
+++ app/src/main/java/pe/edu/esan/appesan2/Mapa.java
@@ -5,7 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -14,7 +22,7 @@ import com.google.android.gms.maps.MapView;
 public class Mapa extends Fragment {
 
     MapView m;
-
+    private GoogleMap googleMap;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -22,6 +30,32 @@ public class Mapa extends Fragment {
         View v = inflater.inflate(R.layout.lay_mapa, container, false);
         m = (MapView) v.findViewById(R.id.mapView);
         m.onCreate(savedInstanceState);
+        m.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = m.getMap();
+        // latitude and longitude
+        double latitude = -12.105381;
+        double longitude = -76.961944;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(-12.105381,-76.961944 )).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
 
         return v;
     }
