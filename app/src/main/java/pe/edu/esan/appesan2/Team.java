02efc6fe984cf1fe1b19944813ae5df7f4e75427
diegoflg <diegoflg2@hotmail.com>
2015-07-16 15:57:55 -0500

    Cafeteria jala ddatos de googl drivev2

---
+++ app/src/main/java/pe/edu/esan/appesan2/Team.java
@@ -0,0 +1,65 @@
+package pe.edu.esan.appesan2;
+
+/**
+ * Created by Diegoflg on 7/16/2015.
+ */
+public class Team {
+    private String dia;
+    private String entrada;
+    private String plato;
+    private String postre;
+    private String refresco;
+
+    public Team(String dia,String entrada, String plato, String postre, String refresco)
+    {
+        this.setDia(dia);
+        this.setEntrada(entrada);
+        this.setPlato(plato);
+        this.setPostre(postre);
+        this.setRefresco(refresco);
+
+    }
+
+    public String getdDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+
+        this.dia = dia;
+    }
+
+    public String getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+
+    public String getPlato() {
+        return plato;
+    }
+
+    public void setPlato(String plato) {
+        this.plato = plato;
+    }
+
+    public String getPostre() {
+        return postre;
+    }
+
+    public void setPostre(String postre) {
+        this.postre = postre;
+    }
+
+    public String getRefresco() {
+        return refresco;
+    }
+
+    public void setRefresco(String refresco) {
+        this.refresco = refresco;
+    }
+
+
+}
---
+++ app/src/main/java/pe/edu/esan/appesan2/TeamsAdapter.java
@@ -0,0 +1,50 @@
+package pe.edu.esan.appesan2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Diegoflg on 7/16/2015.
+ */public class TeamsAdapter extends ArrayAdapter<Team> {
+
+    Context context;
+    private ArrayList<Team> teams;
+
+    public TeamsAdapter(Context context, int textViewResourceId, ArrayList<Team> items) {
+        super(context, textViewResourceId, items);
+        this.context = context;
+        this.teams = items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.team, null);
+        }
+        Team o = teams.get(position);
+        if (o != null) {
+            TextView dia = (TextView) v.findViewById(R.id.position);
+            TextView entrada = (TextView) v.findViewById(R.id.name);
+            TextView plato = (TextView) v.findViewById(R.id.wins);
+            TextView postre = (TextView) v.findViewById(R.id.draws);
+            TextView refresco = (TextView) v.findViewById(R.id.losses);
+
+
+            dia.setText(String.valueOf(o.getdDia()));
+            entrada.setText(String.valueOf(o.getEntrada()));
+            plato.setText(String.valueOf(o.getPlato()));
+            postre.setText(String.valueOf(o.getPostre()));
+            refresco.setText(String.valueOf(o.getRefresco()));
+
+        }
+        return v;
+    }
+}
