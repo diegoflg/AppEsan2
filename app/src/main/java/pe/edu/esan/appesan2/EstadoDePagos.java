6ddec5ab47ab65622963c50424005232ef8a5812
diegoflg <diegoflg2@hotmail.com>
2015-07-24 15:15:23 -0500

    Estado

---
+++ app/src/main/java/pe/edu/esan/appesan2/EstadoDePagos.java
@@ -0,0 +1,28 @@
+package pe.edu.esan.appesan2;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Diegoflg on 7/24/2015.
+ */
+public class EstadoDePagos extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.lay_horario, container, false);
+
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        return rootView;
+
+
+    }
+}
