5d90c8740875813b9748305e40c252c14a8460db
FiorelaDelPilar <fiorela2496@gmail.com>
2015-07-30 10:35:48 -0500

    Impresiones FileChooser

--- app/src/main/java/pe/edu/esan/appesan2/Impresiones.java
+++ app/src/main/java/pe/edu/esan/appesan2/Impresiones.java
@@ -1,22 +1,39 @@
 package pe.edu.esan.appesan2;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+
 
 /**
  * Created by educacionadistancia on 20/07/2015.
  */
 public class Impresiones extends Fragment {
+    public Uri imageUri;
+
+    private static final int FILECHOOSER_RESULTCODE   = 2888;
+    private ValueCallback<Uri> mUploadMessage;
+    private Uri mCapturedImageURI = null;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
         View rootView = inflater.inflate(R.layout.lay_impresiones, container, false);
         WebView myWebView = (WebView) rootView.findViewById(R.id.webviewI);
@@ -25,16 +42,92 @@ public class Impresiones extends Fragment {
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.getSettings().setLoadWithOverviewMode(true);
         myWebView.getSettings().setBuiltInZoomControls(true);
-        //myWebView.getSettings().setDomStorageEnabled(true);
         myWebView.getSettings().setSupportZoom(true);
+
+        myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        myWebView.setScrollbarFadingEnabled(false);
+        myWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        myWebView.getSettings().setAllowFileAccess(true);
         myWebView.setInitialScale(50);
 
-        myWebView.setOnKeyListener(new View.OnKeyListener() {
+        myWebView.setWebChromeClient(new WebChromeClient() {
+
+            // openFileChooser for Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType){
+
+                // Update message
+                mUploadMessage = uploadMsg;
+
+                try{
+
+                    // Create AndroidExampleFolder at sdcard
+
+                    File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "AndroidExampleFolder");
+                    if (!imageStorageDir.exists()) {
+                        // Create AndroidExampleFolder at sdcard
+                        imageStorageDir.mkdirs();
+                    }
+
+                    // Create camera captured image file path and name
+                    File file = new File(
+                            imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    mCapturedImageURI = Uri.fromFile(file);
+
+                    // Camera capture image intent
+                    final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+
+                    Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                    i.addCategory(Intent.CATEGORY_OPENABLE);
+                    i.setType("image/*");
+
+                    // Create file chooser intent
+                    Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+
+                    // Set camera intent to file chooser
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[] { captureIntent });
+
+                    // On select image call onActivityResult method of activity
+                    startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+                }
+                catch(Exception e){}
+            }
+
+            // openFileChooser for Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsg){openFileChooser(uploadMsg, "");
+            }
+
+            //openFileChooser for other Android versions
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+
+
+            // The webPage has 2 filechoosers and will send a
+            // console message informing what action to perform,
+            // taking a photo or updating the file
+
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+
+                onConsoleMessage(cm.message(), cm.lineNumber(), cm.sourceId());
+                return true;
+            }
+
+            public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+                //Log.d("androidruntime", "Show console messages, Used for debugging: " + message);
+
+            }
+        });   // End setWebChromeClient
+
+
+
+
+    myWebView.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN) {
                     WebView webView = (WebView) v;
-
                     switch (keyCode) {
                         case KeyEvent.KEYCODE_BACK:
                             if (webView.canGoBack()) {
@@ -44,27 +137,23 @@ public class Impresiones extends Fragment {
                             break;
                     }
                 }
-
                 return false;
             }
         });
 
         myWebView.setWebViewClient(new WebViewClient() {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return false;
-        };
-        //http://stackoverflow.com/questions/16055800/how-to-enter-password-automatically-in-webview
-        public void onPageFinished(WebView view, String url) {
-            //view.loadUrl("javascript:document.getElementsByName('Username')[0].value = '" + "14100015" + "';document.getElementsByName('Password')[0].value='" + "N7N2U2F7" + "';");
-            //view.loadUrl("javascript:document.forms[0].login()");
-            view.loadUrl("javascript:document.getElementsByName('Username')[0].value = '14100015'");
-            view.loadUrl("javascript:document.getElementsByName('Password')[0].value = 'N7N2U2F7'");
-            view.loadUrl("javascript:document.forms['loginform'].submit()");
-        }
-        }
-        );
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            };
+            //http://stackoverflow.com/questions/16055800/how-to-enter-password-automatically-in-webview
+            public void onPageFinished(WebView view, String url) {
+                view.loadUrl("javascript:document.getElementsByName('Username')[0].value = '14100015'");
+                view.loadUrl("javascript:document.getElementsByName('Password')[0].value = 'N7N2U2F7'");
+                view.loadUrl("javascript:document.forms['loginform'].submit()");
+            }}
 
+        );
         return rootView;
     }
 }
