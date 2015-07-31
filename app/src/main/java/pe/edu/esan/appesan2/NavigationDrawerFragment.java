347434668ce500a3d0da88c00c89ad088197baee
diegoflg <diegoflg2@hotmail.com>
2015-07-31 12:34:12 -0500

    responsivev2

--- app/src/main/java/pe/edu/esan/appesan2/NavigationDrawerFragment.java
+++ app/src/main/java/pe/edu/esan/appesan2/NavigationDrawerFragment.java
@@ -98,6 +98,8 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
+
         mDrawerListView = (ListView) inflater.inflate(
                 R.layout.fragment_navigation_drawer, container, false);
         mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
