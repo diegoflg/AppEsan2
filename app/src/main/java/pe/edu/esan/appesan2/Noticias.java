74497cf348e78779b0c272c9793826c343a74e25
FiorelaDelPilar <fiorela2496@gmail.com>
2015-08-20 17:33:52 -0500

    intento de rss 2

---
+++ app/src/main/java/pe/edu/esan/appesan2/Noticias.java
@@ -0,0 +1,66 @@
+package pe.edu.esan.appesan2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by educacionadistancia on 20/08/2015.
+ */
+public class Noticias implements Serializable{
+    private String titulo;
+    private String contenido;
+    private String resumen;
+    private String enlace;
+    private String imagen;
+    private Date fecha;
+
+    public void setTitulo(String title) {
+        this.titulo = title;
+    }
+
+    public void setContenido(String content) {
+        this.contenido = content;
+    }
+
+    public void setResumen(String text) {
+        this.resumen = text;
+    }
+
+    public void setEnlace(String url) {
+        this.enlace = url;
+    }
+
+    public void setImage(String image) {
+        this.imagen = image;
+    }
+
+    public void setFecha(Date date) {
+        this.fecha = date;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getContenido() {
+        return this.contenido;
+    }
+
+    public String getResumen() {
+        return this.resumen;
+    }
+
+    public String getEnlace() {
+        return this.enlace;
+    }
+
+    public String getImagen() {
+        return this.imagen;
+    }
+
+    public Date getFecha() {
+        return this.fecha;
+    }
+}
+
+
---
+++ app/src/main/java/pe/edu/esan/appesan2/Noticias_Adapter.java
@@ -0,0 +1,79 @@
+package pe.edu.esan.appesan2;
+
+import java.util.ArrayList;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Noticias_Adapter extends ArrayAdapter<Object> {
+	Context context;
+	private ArrayList<Noticias> noticias;
+	RequestQueue requestQueue;
+	ImageLoader imageLoader;
+
+	public Noticias_Adapter(Context context, ArrayList<Noticias> noticias) {
+		super(context, R.layout.item_noticias);
+		this.context = context;
+		this.noticias = noticias;
+		
+		requestQueue = Volley.newRequestQueue(context);
+		imageLoader = new ImageLoader(requestQueue, new BitmapCache(10));
+	}
+
+	@Override
+	public int getCount() {
+		return noticias.size();
+	}
+
+	private static class PlaceHolder {
+
+		TextView title;
+		TextView time;
+		TextView content;
+		
+		ImageView picture;
+
+		public static PlaceHolder generate(View convertView) {
+			PlaceHolder placeHolder = new PlaceHolder();
+			placeHolder.title = (TextView) convertView
+					.findViewById(R.id.noticia_textview_title);
+			placeHolder.time = (TextView) convertView
+					.findViewById(R.id.noticia_textview_time);
+			placeHolder.content = (TextView) convertView
+					.findViewById(R.id.noticia_textview_content);
+			
+			placeHolder.picture = (ImageView) convertView
+					.findViewById(R.id.noticia_imageView);
+			return placeHolder;
+		}
+
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		PlaceHolder placeHolder;
+		if (convertView == null) {
+			convertView = View.inflate(context, R.layout.item_noticias, null);
+			placeHolder = PlaceHolder.generate(convertView);
+			convertView.setTag(placeHolder);
+		} else {
+			placeHolder = (PlaceHolder) convertView.getTag();
+		}
+		placeHolder.title.setText(noticias.get(position).getTitulo());
+		placeHolder.time.setText(""
+				+ noticias.get(position).getFecha().getDate() + "/"
+				+ noticias.get(position).getFecha().getMonth());
+		placeHolder.content.setText(noticias.get(position).getResumen());
+		
+		imageLoader.get(noticias.get(position).getImagen(), ImageLoader.getImageListener(placeHolder.picture, R.drawable.ic_launcher, R.drawable.ic_launcher));
+		return (convertView);
+	}
+
+}
