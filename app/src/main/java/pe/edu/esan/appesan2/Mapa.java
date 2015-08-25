45ce499eb2a3e73c6a87ca65e840a0d5cd86d1b3
FiorelaDelPilar <fiorela2496@gmail.com>
2015-08-25 14:07:34 -0500

    Mapa de la universidad con zoom y drag

--- app/src/main/java/pe/edu/esan/appesan2/Mapa.java
+++ app/src/main/java/pe/edu/esan/appesan2/Mapa.java
@@ -4,13 +4,21 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.FloatMath;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,6 +34,26 @@ public class Mapa extends Fragment {
     private GoogleMap googleMap;
     MapView m;
     TabHost mTabHost3;
+    ImageView imagenMapa;
+
+    private  final String TAG = "APP";
+    EditText busca;
+    // These matrices will be used to move and zoom image
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    // We can be in one of these 3 states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    // Remember some things for zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+    String savedItemClicked;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +74,11 @@ public class Mapa extends Fragment {
         spec.setIndicator("Waze");
         mTabHost3.addTab(spec);
 
+        spec=mTabHost3.newTabSpec("Tab 3");
+        spec.setContent(R.id.mapaesan);
+        spec.setIndicator("MapaEsan");
+        mTabHost3.addTab(spec);
+
         mTabHost3.setCurrentTab(0);
 
         //FUENTE PARA TÍTULO EN TABHOST:
@@ -64,6 +97,7 @@ public class Mapa extends Fragment {
         }
         mTabHost3.getTabWidget().getChildAt(0).setBackgroundColor(Color.parseColor("#ffc90039")); //unselected
         mTabHost3.getTabWidget().getChildAt(1).setBackgroundColor(Color.parseColor("#ffc90039")); // selected
+        mTabHost3.getTabWidget().getChildAt(2).setBackgroundColor(Color.parseColor("#ffc90039")); // selected
 
         m = (MapView) v.findViewById(R.id.mapView);
         m.onCreate(savedInstanceState);
@@ -89,15 +123,124 @@ public class Mapa extends Fragment {
         CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(-12.105019, -76.961066)).zoom(18).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+
+        //---------------------------------MAPA DE ESAN-------------------------------------
+
+        //FUENTE Y COLOR PARA BUSCAR EN TEXTVIEW:
+        String font_pathL = "font/HelveticaNeue-Bold.ttf"; //ruta de la fuente
+        Typeface TFL = Typeface.createFromAsset(getActivity().getAssets(),font_pathL);//llamanos a la CLASS TYPEFACE y la definimos con un CREATE desde ASSETS con la ruta STRING
+
+        imagenMapa = (ImageView)v.findViewById(R.id.imagenMapa);
+        busca = (EditText)v.findViewById(R.id.buscaLugar);
+        busca.setTypeface(TFL);
+        busca.setTextColor(Color.parseColor("#FFFFFF"));
+        //----------------------------------------------------------------------------------
+
+
         mTabHost3.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
             public void onTabChanged(String tabId) {
-                if(mTabHost3.getCurrentTab()==1){
+                if (mTabHost3.getCurrentTab() == 1) {
                     waze();
+                }else if(mTabHost3.getCurrentTab()==2){
+                    //--------------------------ESAN MAPA-------------------------------
+                    imagenMapa.setImageResource(R.drawable.mapaesan);
+
+                    imagenMapa.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            dumpEvent(event);
+
+                            // Handle touch events here...
+                            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                                case MotionEvent.ACTION_DOWN:
+                                    savedMatrix.set(matrix);
+                                    start.set(event.getX(), event.getY());
+                                    Log.d(TAG, "mode=DRAG");
+                                    mode = DRAG;
+                                    break;
+                                case MotionEvent.ACTION_POINTER_DOWN:
+                                    oldDist = spacing(event);
+                                    Log.d(TAG, "oldDist=" + oldDist);
+                                    if (oldDist > 10f) {
+                                        savedMatrix.set(matrix);
+                                        midPoint(mid, event);
+                                        mode = ZOOM;
+                                        Log.d(TAG, "mode=ZOOM");
+                                    }
+                                    break;
+                                case MotionEvent.ACTION_UP:
+                                case MotionEvent.ACTION_POINTER_UP:
+                                    mode = NONE;
+                                    Log.d(TAG, "mode=NONE");
+                                    break;
+                                case MotionEvent.ACTION_MOVE:
+                                    if (mode == DRAG) {
+                                        // ...
+                                        matrix.set(savedMatrix);
+                                        matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                                    } else if (mode == ZOOM) {
+                                        float newDist = spacing(event);
+                                        Log.d(TAG, "newDist=" + newDist);
+                                        if (newDist > 10f) {
+                                            matrix.set(savedMatrix);
+                                            float scale = newDist / oldDist;
+                                            matrix.postScale(scale, scale, mid.x, mid.y);
+                                        }
+                                    }
+                                    break;
+                            }
+
+                            imagenMapa.setImageMatrix(matrix);
+                            return true;
+                        }
+                    });
                 }
             }
         });
+
+
         return v;
+    }
+
+    private void dumpEvent(MotionEvent event) {
+        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE",
+                "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
+        StringBuilder sb = new StringBuilder();
+        int action = event.getAction();
+        int actionCode = action & MotionEvent.ACTION_MASK;
+        sb.append("event ACTION_").append(names[actionCode]);
+        if (actionCode == MotionEvent.ACTION_POINTER_DOWN
+                || actionCode == MotionEvent.ACTION_POINTER_UP) {
+            sb.append("(pid ").append(
+                    action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+            sb.append(")");
+        }
+        sb.append("[");
+        for (int i = 0; i < event.getPointerCount(); i++) {
+            sb.append("#").append(i);
+            sb.append("(pid ").append(event.getPointerId(i));
+            sb.append(")=").append((int) event.getX(i));
+            sb.append(",").append((int) event.getY(i));
+            if (i + 1 < event.getPointerCount())
+                sb.append(";");
+        }
+        sb.append("]");
+        Log.d(TAG, sb.toString());
+    }
+
+    /** Determine the space between the first two fingers */
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return FloatMath.sqrt(x * x + y * y);
+    }
+
+    /** Calculate the mid point of the first two fingers */
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
     }
 
     @Override
@@ -128,7 +271,8 @@ public class Mapa extends Fragment {
     public void waze(){
         try
         {
-            String url = "waze://?ll=-12.104942500,-76.961065500&z=10";
+            //String url = "waze://?ll=<-12.105019>,<-76.961066>&z=10";
+            String url = "waze://?ll=-12.105,-76.961&z=10";
             Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse(url) );
             startActivity( intent );
         }
@@ -138,5 +282,8 @@ public class Mapa extends Fragment {
             startActivity(intent);
         }
     }
+
+
 }
+
 
