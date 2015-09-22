d0fa1e731d0e2371208c45b1e2f77b38bfb189db
FiorelaDelPilar <fiorela2496@gmail.com>
2015-09-22 17:25:56 -0500

    Módulo de mapas sin GoogleMaps (layout)

--- app/src/main/java/pe/edu/esan/appesan2/Mapa.java
+++ app/src/main/java/pe/edu/esan/appesan2/Mapa.java
@@ -29,16 +29,6 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-
 public class Mapa extends Fragment {
     TabHost mTabHost3;
     ImageView imagenMapa;
