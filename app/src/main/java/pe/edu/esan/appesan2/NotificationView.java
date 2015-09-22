9a403ae051e849c3a78e8fa13fd28654e0d59639
FiorelaDelPilar <fiorela2496@gmail.com>
2015-09-22 10:11:02 -0500

    notificacion subida

---
+++ app/src/main/java/pe/edu/esan/appesan2/NotificationView.java
@@ -0,0 +1,15 @@
+package pe.edu.esan.appesan2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by educacionadistancia on 21/09/2015.
+ */
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
